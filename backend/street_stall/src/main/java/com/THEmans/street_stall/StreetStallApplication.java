15785d31b0aaa203a3680fd92e61bb94438d04be
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2022-05-29 22:37:09 +0900

    #2 [CHORE] : add project setting
    백엔드 프로젝트 생성 및 초기 세팅

---
+++ backend/street_stall/src/main/java/com/THEmans/street_stall/StreetStallApplication.java
@@ -0,0 +1,13 @@
+package com.THEmans.street_stall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetStallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetStallApplication.class, args);
+	}
+
+}
